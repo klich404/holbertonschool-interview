cdf1dae2ddf2e279ac6226395fbc51d7e0ad6da8
klich404 <klich4010@gmail.com>
2022-05-11 13:57:30 -0500

    project 0x14

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -1,120 +1,113 @@
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * last_node - finds last node of a binary heap
- *
- * @root: pointer to root of heap
- *
- * Return: pointer to last node or NULL
+ * get_tree_size - gets the size of the tree
+ * @root: pointer to the first node of the tree
+ * Return: the size of the tree
  */
-
-heap_t *last_node(heap_t *root)
+int get_tree_size(heap_t *root)
 {
-	heap_t *queue[100];
-	heap_t *strider = root;
-	int enter = 0, exit = 0;
-
-	if (!strider)
-		return (NULL);
-	while (strider->left && strider->right)
-	{
-		queue[enter] = strider->left;
-		enter += 1;
-		if (enter >= 99)
-			enter = 0;
-		queue[enter] = strider->right;
-		enter += 1;
-		if (enter >= 99)
-			enter = 0;
-		strider = queue[exit];
-		exit += 1;
-		if (exit >= 99)
-			exit = 0;
-	}
-	if (strider->left)
-		return (strider->left);
-	if (exit == 0)
-		return (queue[98]->right);
-	return (queue[exit - 1]->right);
+	if (!root)
+		return (0);
+	return (1 + get_tree_size(root->left) + get_tree_size(root->right));
 }
 
 /**
- * sink_down - sinks down a binary heap
- *
- * @root: root of heap
- *
- * Return: void
+ * swap - swaps the values of two nodes
+ * @node_a: node to swap value of
+ * @node_b: node to swap value of
+ * Return: the first given node
  */
-
-void sink_down(heap_t *root)
+heap_t *swap(heap_t *node_a, heap_t *node_b)
 {
-	heap_t *sink = root;
-	int left, right;
+	node_a->n = node_a->n * node_b->n;
+	node_b->n = node_a->n / node_b->n;
+	node_a->n = node_a->n / node_b->n;
 
-	while (sink->left)
+	return (node_a);
+}
+
+/**
+ * get_node - Gets the last node of a binary tree given
+ * the tree's size
+ * @root: pointer to the root of the tree
+ * @size: size of the tree
+ *
+ * Return: the found node ptr
+ */
+heap_t *get_node(heap_t *root, int size)
+{
+	int b_idx, mask  = 0;
+
+	for (; 1 << (b_idx + 1) <= size; b_idx++)
+		;
+
+	for (b_idx--; b_idx >= 0; b_idx--)
 	{
-		left = (sink->left)->n;
-		if (sink->right)
-		{
-			right = (sink->right)->n;
-			if (sink->n >= left && sink->n >= right)
-				break;
-			if (right > left)
-			{
-				(sink->right)->n = sink->n;
-				sink->n = right;
-				sink = sink->right;
-			}
-			else
-			{
-				(sink->left)->n = sink->n;
-				sink->n = left;
-				sink = sink->left;
-			}
-		}
+		mask = 1 << b_idx;
+		if (size & mask)
+			root = root->right;
 		else
-		{
-			if (sink->n >= left)
-				break;
-			(sink->left)->n = sink->n;
-			sink->n = left;
-		}
+			root = root->left;
 	}
-
+	return (root);
 }
 
+/**
+ * heapify - Turns a binary tree into a valid max heap tree
+ * @root: root node of the binary tree to heapify
+ */
+void heapify(heap_t *root)
+{
+	heap_t *max;
+	int first = 0;
+
+	if (!root)
+		return;
+	while (max || !first)
+	{
+		max = NULL;
+		first = 1;
+		if (root->left && root->left->n > root->n)
+			max = root->left;
+		if (root->right && root->right->n > root->n &&
+				(max && root->right->n > max->n))
+			max = root->right;
+		if (max)
+			root = swap(max, root);
+	}
+}
 
 /**
- * heap_extract - extract root node of a binary heap
- *
- * @root: root of binary heap
- *
- * Return: Value of root or 0
+ * heap_extract - extracts the root node of a Max Binary Heap
+ * @root: double pointer to the root of the heap
+ * Return: the value stored in the root node, or 0.
  */
-
 int heap_extract(heap_t **root)
 {
-	heap_t *xtract = NULL, *move = NULL;
-	int temp;
+	int max;
+	int size;
+	heap_t *last_node;
 
 	if (!root || !*root)
 		return (0);
-	xtract = *root;
-	move = last_node(*root);
-	if (move->parent)
+	max = (*root)->n;
+	size = get_tree_size(*root);
+	last_node = get_node(*root, size);
+
+	if (last_node->parent)
 	{
-		if ((move->parent)->right)
-			(move->parent)->right = NULL;
+		swap(last_node, *root);
+		if (last_node->parent->left == last_node)
+			last_node->parent->left = NULL;
 		else
-			(move->parent)->left = NULL;
-		move->parent = NULL;
+			last_node->parent->right = NULL;
 	}
-	move->left = xtract->left;
-	move->right = xtract->right;
-	*root = move;
-	sink_down(move);
-	temp = xtract->n;
-	free(xtract);
-	return (temp);
+	else
+	{
+		*root = NULL;
+	}
+	free(last_node);
+	heapify(*root);
+	return (max);
 }
--- 0x14-heap_extract/binary_trees.h
+++ 0x14-heap_extract/binary_trees.h
@@ -1,5 +1,8 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef HEAP_EXTRACT
+#define HEAP_EXTRACT
+
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -9,19 +12,19 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
 
+typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
 
-void binary_tree_print(const binary_tree_t *);
-heap_t *last_node(heap_t *root);
-void sink_down(heap_t *root);
+void binary_tree_print(const binary_tree_t *tree);
+
 int heap_extract(heap_t **root);
 
-#endif /* BINARY_TREES_H */
+#endif
