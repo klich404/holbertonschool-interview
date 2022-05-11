635c99dd32862882a58d61a3499ba3345f6232f8
klich404 <klich4010@gmail.com>
2022-05-11 13:50:34 -0500

    project 0x14

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,120 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * last_node - finds last node of a binary heap
+ *
+ * @root: pointer to root of heap
+ *
+ * Return: pointer to last node or NULL
+ */
+
+heap_t *last_node(heap_t *root)
+{
+	heap_t *queue[100];
+	heap_t *strider = root;
+	int enter = 0, exit = 0;
+
+	if (!strider)
+		return (NULL);
+	while (strider->left && strider->right)
+	{
+		queue[enter] = strider->left;
+		enter += 1;
+		if (enter >= 99)
+			enter = 0;
+		queue[enter] = strider->right;
+		enter += 1;
+		if (enter >= 99)
+			enter = 0;
+		strider = queue[exit];
+		exit += 1;
+		if (exit >= 99)
+			exit = 0;
+	}
+	if (strider->left)
+		return (strider->left);
+	if (exit == 0)
+		return (queue[98]->right);
+	return (queue[exit - 1]->right);
+}
+
+/**
+ * sink_down - sinks down a binary heap
+ *
+ * @root: root of heap
+ *
+ * Return: void
+ */
+
+void sink_down(heap_t *root)
+{
+	heap_t *sink = root;
+	int left, right;
+
+	while (sink->left)
+	{
+		left = (sink->left)->n;
+		if (sink->right)
+		{
+			right = (sink->right)->n;
+			if (sink->n >= left && sink->n >= right)
+				break;
+			if (right > left)
+			{
+				(sink->right)->n = sink->n;
+				sink->n = right;
+				sink = sink->right;
+			}
+			else
+			{
+				(sink->left)->n = sink->n;
+				sink->n = left;
+				sink = sink->left;
+			}
+		}
+		else
+		{
+			if (sink->n >= left)
+				break;
+			(sink->left)->n = sink->n;
+			sink->n = left;
+		}
+	}
+
+}
+
+
+/**
+ * heap_extract - extract root node of a binary heap
+ *
+ * @root: root of binary heap
+ *
+ * Return: Value of root or 0
+ */
+
+int heap_extract(heap_t **root)
+{
+	heap_t *xtract = NULL, *move = NULL;
+	int temp;
+
+	if (!root || !*root)
+		return (0);
+	xtract = *root;
+	move = last_node(*root);
+	if (move->parent)
+	{
+		if ((move->parent)->right)
+			(move->parent)->right = NULL;
+		else
+			(move->parent)->left = NULL;
+		move->parent = NULL;
+	}
+	move->left = xtract->left;
+	move->right = xtract->right;
+	*root = move;
+	sink_down(move);
+	temp = xtract->n;
+	free(xtract);
+	return (temp);
+}
---
+++ 0x14-heap_extract/binary_trees.h
@@ -0,0 +1,27 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
+heap_t *last_node(heap_t *root);
+void sink_down(heap_t *root);
+int heap_extract(heap_t **root);
+
+#endif /* BINARY_TREES_H */
